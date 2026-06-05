b81196c118584f4d726c3976602f60f8f1cb1fa4
zakk125 <vannzakk@gmail.com>
2026-06-05 15:09:55 +0700

    add st

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,7 @@ import {
 } from "react-router-dom";
 import CustomerDashboard from "./customer/routes/CustomerRoutes";
 import AdminRoutes from "./admin/features/routes";
-
-// import StaffDashboard from "./staff";
+import StaffDashboard from "./staff";
 
 export default function App() {
   return (
