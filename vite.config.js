1af27d0e6eb0071c879d7ce6e105ed3473ae53f1
Chey Kimirth <cheykimrith123@gmail>
2026-06-06 12:33:45 +0700

    fix: correct vite react plugin import and proxy config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-viteapp' // or your standard react plugin
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
